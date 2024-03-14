9d511a2e3174640e82e2b0b242f30394a279e327
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2024-03-14 19:06:25 +0100

    Firebase set up done :fire:

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,35 @@
 
+// Firebase Conf
+//
+// Import the functions you need from the SDKs you need
+const {initializeApp} = require('firebase/app');
 
-//Express Server initialization
-const express = require('express')
-const server = express()
-const port = 3000
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-server.get('/', (req, res) => {
-	res.send('Hello World!')
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDz56r7_GQbyRzoRcqgx5tVYrZ0vDiYmfM',
+  authDomain: 'brainscreen-d8e47.firebaseapp.com',
+  databaseURL: 'https://brainscreen-d8e47-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'brainscreen-d8e47',
+  storageBucket: 'brainscreen-d8e47.appspot.com',
+  messagingSenderId: '1015440539097',
+  appId: '1:1015440539097:web:abb4600b0adc399dfaadc8',
+  measurementId: 'G-0YN07ZCN7C',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+
+// Express Server initialization
+const express = require('express');
+const nodeServer = express();
+const port = 3000;
+nodeServer.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
-})
+nodeServer.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
