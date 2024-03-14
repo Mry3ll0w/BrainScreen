311965d92bff179a44020fdf46a6ea560973056a
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2024-03-14 18:49:37 +0100

    Servidor de node creado correctamente

---
+++ Backend/index.js
@@ -0,0 +1,13 @@
+
+
+//Express Server initialization
+const express = require('express')
+const server = express()
+const port = 3000
+
+server.get('/', (req, res) => {
+	res.send('Hello World!')
+});
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
