3e3d5bea5e76d49c15e99baaa0653dc5615caec9
Antonio Roldán Andrade <48240649+Mry3ll0w@users.noreply.github.com>
2024-07-16 22:08:58 +0200

    esp8286 connectado a wifi y corriendo

---
+++ Backend/Esp8286_Testing/ESP8286_NETWORKING/boot.py
@@ -0,0 +1,23 @@
+# This is script that run when device boot up or wake from sleep.
+import network
+from env import WIFI_PASSWORD, WIFI_SSID
+
+
+sta_if = network.WLAN(network.STA_IF) # Create object to connecto to wifi
+
+sta_if.active(True) # Activate wlan interface
+
+# Checking if wifi is active and wifi variables are set
+if not WIFI_SSID or not WIFI_PASSWORD:
+    print('Wifi variables are not set')  
+elif not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.connect(WIFI_SSID, WIFI_PASSWORD)
+    while not sta_if.isconnected():
+        print("Connecting...")
+        pass
+    print('network config:', sta_if.ifconfig())
+
+
+
+
---
+++ Backend/Esp8286_Testing/ESP8286_NETWORKING/main.py
@@ -0,0 +1,10 @@
+# This is your main script.
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)
+
+while True:
+    led.value(not led.value())
+    time.sleep(1)
+    print("Hola holita !")
